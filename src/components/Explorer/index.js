893e0bc72904bd7377afe036617de84bac198a5b
Artem Ivanov <fast0490f@mail.ru>
2019-09-27 12:52:59 +0300

    Explorer and AppToolBar

--- src/components/Explorer/index.js
+++ src/components/Explorer/index.js
@@ -12,7 +12,7 @@ import { useSpring, animated } from 'react-spring';
 
 const styles = {
   box: {
-    width: 190,
+    width: 200,
     height: '100%',
     backgroundColor: '#ECEFF1',
     padding: 6,
@@ -23,8 +23,11 @@ const useStyles = makeStyles({
   root: {
     height: '100%',
     flexGrow: 1,
-    maxWidth: 190,
+    maxWidth: 200 - 12,
   },
+  itemLabel: {
+    fontSize: '0.9rem',
+  }
 });
 
 const StyledTreeItem = withStyles(theme => ({
@@ -91,18 +94,18 @@ function Explorer() {
         defaultExpandIcon={<PlusSquare />}
         defaultEndIcon={<CloseSquare />}
       >
-        <StyledTreeItem nodeId="2" label="Hello" />
-        <StyledTreeItem nodeId="3" label="Subtree with children">
-          <StyledTreeItem nodeId="6" label="Hello" />
-          <StyledTreeItem nodeId="7" label="Sub-subtree with children">
-            <StyledTreeItem nodeId="9" label="Child 1" />
-            <StyledTreeItem nodeId="10" label="Child 2" />
-            <StyledTreeItem nodeId="11" label="Child 3" />
+        <StyledTreeItem classes={{ label: classes.itemLabel }} nodeId="2" label="Hello" />
+        <StyledTreeItem classes={{ label: classes.itemLabel }} nodeId="3" label="Subtree with children">
+          <StyledTreeItem classes={{ label: classes.itemLabel }} nodeId="6" label="Hello" />
+          <StyledTreeItem classes={{ label: classes.itemLabel }} nodeId="7" label="Sub-subtree with children">
+            <StyledTreeItem classes={{ label: classes.itemLabel }} nodeId="9" label="Child 1" />
+            <StyledTreeItem classes={{ label: classes.itemLabel }} nodeId="10" label="Child 2" />
+            <StyledTreeItem classes={{ label: classes.itemLabel }} nodeId="11" label="Child 3" />
           </StyledTreeItem>
-          <StyledTreeItem nodeId="8" label="Hello" />
+          <StyledTreeItem classes={{ label: classes.itemLabel }} nodeId="8" label="Hello" />
         </StyledTreeItem>
-        <StyledTreeItem nodeId="4" label="World" />
-        <StyledTreeItem nodeId="5" label="Something something" />
+        <StyledTreeItem classes={{ label: classes.itemLabel }} nodeId="4" label="World" />
+        <StyledTreeItem classes={{ label: classes.itemLabel }} nodeId="5" label="Something something" />
       </TreeView>
     </Box>
   );
