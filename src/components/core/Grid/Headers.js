b210db55aa96fc247c19b88228d02f2cf1827972
Artem Ivanov <fast0490f@mail.ru>
2019-10-08 16:03:56 +0300

    core grid basic style

--- src/components/core/Grid/Headers.js
+++ src/components/core/Grid/Headers.js
@@ -7,22 +7,21 @@ import css from './main.module.css';
 
 function Headers({ scroll, size, rowHeight, columnWidth, render }) {
   return (
-    <div style={{ height: rowHeight, width: size.width - scrollbarSize()}}>
-    <Grid
-      tabIndex={null}
-      width={size.width - scrollbarSize()}
-      height={rowHeight}
-      className={css.HeaderGrid}
-      columnWidth={columnWidth}
-      columnCount={50}
-      overscanColumnCount={0}
-      cellRenderer={render}
-      rowHeight={rowHeight}
-      rowCount={1}
-      scrollLeft={scroll.scrollLeft}
-      
-    />
-  </div>
+    <div className={css.HeaderContainer} style={{ height: rowHeight, width: size.width - scrollbarSize()}}>
+      <Grid
+        tabIndex={null}
+        width={size.width - scrollbarSize()}
+        height={rowHeight}
+        className={css.HeaderGrid}
+        columnWidth={columnWidth}
+        columnCount={50}
+        overscanColumnCount={0}
+        cellRenderer={render}
+        rowHeight={rowHeight}
+        rowCount={1}
+        scrollLeft={scroll.scrollLeft}
+      />
+    </div>
   )
 }
 
