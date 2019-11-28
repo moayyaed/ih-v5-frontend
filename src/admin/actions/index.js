7ca261241986328e8c4d0e593d8506c31d498b6f
Artem Ivanov <fast0490f@mail.ru>
2019-11-28 11:44:36 +0300

    actions

--- src/admin/actions/index.js
+++ src/admin/actions/index.js
@@ -1,12 +1,1 @@
-import core from 'core';
-
-
-core.action.appmenu = function(data) {
-  core.components.appmenu.setData({
-    list: data,
-  });
-}
-
-core.action.appnav = function(data) {
-
-}
+import './app';
