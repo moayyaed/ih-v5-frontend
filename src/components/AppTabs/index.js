90d5ad8a5a763e07de8c08b655666428415e50c7
Artem Ivanov <fast0490f@mail.ru>
2019-12-12 14:20:15 +0300

    app tabs

--- src/components/AppTabs/index.js
+++ src/components/AppTabs/index.js
@@ -10,6 +10,8 @@ import CloseIcon from '@material-ui/icons/Close';
 
 import css from './main.module.css';
 
+const MIN_WIDTH_COLUMN = 75;
+
 
 const styles = {
   root: {
@@ -50,6 +52,32 @@ const styles = {
     paddingRight: 34,
     cursor: 'pointer',
     backgroundColor: '#fafafa',
+    zIndex: 100,
+  },
+  fake: {
+    position: 'absolute',
+    display: 'none',
+    zIndex: 99,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  fake2: {
+    border: '2px dashed #E0E0E0', 
+    width: '100%', 
+    height: '100%',
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+  },
+  fake3: {
+    position: 'absolute',
+    marginTop: 3,
+    marginBottom: 0,
+    left: 0,
+    width: '100%', 
+    height: 21,
+  },
+  fakeText: {
+    position: 'absolute',
+    color: '#9E9E9E'
   },
   icon: {
     position: 'absolute',
@@ -72,8 +100,9 @@ function Tab(props) {
   return (
   <Draggable
     axis='x'
-    handle="strong"
     position={{ x: 0, y: 0 }}
+    onStart={(e, data) => props.onClick(e, 'dragstart', props.item, data, props.index)}
+    onStop={(e, data) => props.onClick(e, 'dragstop', props.item, data, props.index)}
   >
     <div 
       className={css.tab} 
@@ -96,11 +125,114 @@ function Tab(props) {
 
 class AppTabs extends Component {
 
-  handleClick = (e, type, item) => {
+  linkBox = (e) => {
+    this.box = e;
+  }
+
+  linkFake= (e) => {
+    this.fake = e;
+  }
+
+  linkFakeText= (e) => {
+    this.fakeText = e;
+  }
+
+  handleDragStart = (e, item, data, index) => {
+    const temp = [];
+    let lastWidth = 0;
+
+    Array.from(this.box.children).forEach((i, key)=> {
+      if (key === 0) {
+        temp.push(lastWidth)
+      } else {
+        temp.push(lastWidth)
+      }
+      lastWidth = lastWidth + i.offsetWidth;
+    })
+
+    const curentColumnWidth = temp[index] + data.x;
+    this.fake.style.width = `${data.node.offsetWidth + 1}px`;
+    this.fake.style.transform = `translateX(${curentColumnWidth -1}px)`;
+    this.fake.style.height = '35px';
+    this.fake.style.display = 'flex';
+    this.fakeText.innerText = item.label;
+  }
+
+  handleDragStop = (e, item, data, index) => {
+    this.fake.style.display = 'none';
+    this.fakeText.innerText = '';
+
+    const temp = [];
+    let lastWidth = 0;
+    let targetColumnIndex = null;
+
+    Array.from(this.box.children).forEach((i, key)=> {
+      if (key === 0) {
+        temp.push(lastWidth)
+      } else {
+        temp.push(lastWidth)
+      }
+      lastWidth = lastWidth + i.offsetWidth;
+    })
+
+    const curentColumnWidth = temp[index] + data.x;
+
+    temp.forEach((i, key) => {
+      if (targetColumnIndex === null && i - MIN_WIDTH_COLUMN > curentColumnWidth) {
+        targetColumnIndex = key - 1;
+      }
+    });
+
+    if (targetColumnIndex === null) {
+      targetColumnIndex = temp.length - 1;
+    }
+    if (targetColumnIndex === -1) {
+      targetColumnIndex = 0;
+    }
+
+    if (targetColumnIndex !== index) {
+      const temp2 = this.props.state.list.reduce((l, n, key, arr) => {
+        if (key === targetColumnIndex && key === index) {
+          return l.concat(n);
+        } 
+        if (key === targetColumnIndex) {
+          if (targetColumnIndex > index) {
+            return l.concat(n, arr[index]);
+          }
+          if (targetColumnIndex < index) {
+            return l.concat(arr[index], n);
+          }
+          return l.concat(n);
+        }
+        if (key === index) {
+          return l;
+        }
+        return l.concat(n);
+      }, []);
+      
+  
+      
+      core.components.apptabs.setData({
+        ...this.props.state,
+        list: temp2,
+      });
+    }
+  }
+
+  handleClick = (e, type, item, data, index) => {
     e.preventDefault();
     e.stopPropagation();
 
     const { menuid, navcomponent } = core.nav.state;
+
+    if (type === 'dragstart') {
+      core.nav.history.push(`/${menuid}/${navcomponent}/${item.id}`);
+      this.handleDragStart(e, item, data, index);
+    }
+
+    if (type === 'dragstop') {
+      this.handleDragStop(e, item, data, index);
+    }
     
     if (type === 'select') {
       core.nav.history.push(`/${menuid}/${navcomponent}/${item.id}`);
@@ -123,12 +255,18 @@ class AppTabs extends Component {
   render({ id, state, classes } = this.props) {
     return (
       <div style={this.props.data.length === 0 ? styles.root : styles.rootA }>
-        <div style={styles.box}>
-          {state.list.map(i => 
+        <div ref={this.linkFake} style={styles.fake}>
+          <div ref={this.linkFakeText} style={styles.fakeText} />
+          <div style={styles.fake3} />
+          <div style={styles.fake2} />
+        </div>
+        <div ref={this.linkBox} style={styles.box}>
+          {state.list.map((i, k) => 
             <Tab 
-              key={i.id} 
-              select={state.selectid}
+              key={i.id}
+              index={k}
               item={i} 
+              select={state.selectid}
               onClick={this.handleClick}
             />)}
         </div>
