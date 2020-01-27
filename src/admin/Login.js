3c73214e8e32c45d7dcc79959cf4eb63038d253a
Artem Ivanov <fast0490f@mail.ru>
2020-01-27 16:46:14 +0300

    core 3.13.5

--- src/admin/Login.js
+++ src/admin/Login.js
@@ -13,7 +13,8 @@ const styles = {
     height: '100%',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundSize: 'auto',
+    background: 'linear-gradient(135deg, rgba(255, 255, 255, 0.95) 0%, rgba(255, 255, 255, 0.95) 50%), url(https://cdn.nextgov.com/media/img/upload/2020/01/14/NGiot20200114/860x394.jpg) no-repeat center center',
+    backgroundSize: 'cover',
   },
   page: {
     position: 'relative',
@@ -21,11 +22,37 @@ const styles = {
     height: 425,
     borderRadius: 3,
     backgroundColor: 'white',
-    backgroundSize: 'cover',
     overflow: 'hidden',
     boxShadow: '0px 53px 37px -39px rgba(0, 0, 0, 0.7)',
+    background: 'linear-gradient(45deg, rgba(96, 125, 139, 0.6) 0%, rgba(96, 125, 139, 0.9) 100%), url(https://cdn.nextgov.com/media/img/upload/2020/01/14/NGiot20200114/860x394.jpg) no-repeat center center',
+    backgroundSize: 'cover',
   },
-  panel: {
+  panel1: {
+    display: 'flex',
+    position: 'absolute',
+    width: '50%',
+    height: '100%',
+    left: 0,
+    padding: 25,
+    flexDirection: 'column',
+  },
+  logoText1: {
+    color: '#fff',
+    fontSize: 18,
+  },
+  logoText2: {
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  logoText3: {
+    position: 'absolute',
+    bottom: 6,
+  },
+  url: {
+    color: '#e8e8e8',
+  },
+  panel2: {
     display: 'flex',
     position: 'absolute',
     width: '50%',
@@ -43,8 +70,27 @@ const styles = {
 
   },
   header: {
-    color: '#252525',
+    marginTop: 18,
+    marginBottom: 18,
+  },
+  headerText: {
+    color: '#878787',
     fontSize: 18,
+    height: 28,
+    // borderBottom: '2px solid #e8e8e8',
+  },
+  headerBorder: {
+    background: '#9E9E9E',
+    height: 4,
+    width: 46,
+  },
+  button: {
+    marginTop: 24, 
+    marginBottom: 24,
+  },
+  text: {
+    marginTop: 6, 
+    marginBottom: 6,
   }
 }
 
@@ -52,22 +98,41 @@ function Login() {
   return (
     <div style={styles.root}>
       <div style={styles.page}>
-        <div style={styles.panel}>
+        <div style={styles.panel1}>
+          <div style={styles.logoText1}>Welcome to</div>
+          <div style={styles.logoText2}>IH-SYSTEMS</div>
+          <div style={styles.logoText3}><a style={styles.url}>ih-systems.com | 5.1.0</a></div>
+        </div>
+        <div style={styles.panel2}>
           <div style={styles.header}>
-            Sign in
+            <div style={styles.headerText}>Login</div>
+            <div style={styles.headerBorder} />
           </div>
           <div style={styles.form}>
-            <TextField variant="outlined" style={{ marginTop: 6, marginBottom: 6 }} fullWidth id="username" label="Username" />
+            <TextField 
+              variant="filled"  
+              fullWidth 
+              id="username" 
+              label="Username" 
+              style={styles.text} 
+            />
             <TextField
               fullWidth
-              variant="outlined"
+              variant="filled"
               id="password"
               label="Password"
               type="password"
               autoComplete="current-password"
-              style={{ marginTop: 6, marginBottom: 6 }}
+              style={styles.text}
             />
-            <Button fullWidth style={{ marginTop: 24, marginBottom: 24 }} variant="outlined">Enter</Button>
+            <Button 
+              fullWidth
+              variant="outlined"
+              style={styles.button}
+              onClick={() => core.init()}
+            >
+              Enter
+            </Button>
           </div>
         </div>
       </div>
--- src/admin/theme.js
+++ src/admin/theme.js
@@ -2,12 +2,17 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 
 const theme = createMuiTheme({
+
+});
+
+
+export default theme;
+
+
+/*
   palette: {
     primary: {
       main: '#F5F5F5',
     }
   }
-});
-
-
-export default theme;
+  */
