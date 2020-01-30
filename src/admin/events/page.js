f4ec279127fb70110cd30c1548e332c61bd9fd49
Artem Ivanov <fast0490f@mail.ru>
2020-01-30 16:38:04 +0300

    core 3.14.22

--- src/admin/events/page.js
+++ src/admin/events/page.js
@@ -3,72 +3,66 @@ import core from 'core';
 
 function cache(params) {
   const store = core.store.getState();
-  
-  if (core.nav.last.navid !== null && core.nav.last.navcomponent !== null) {
-    const name = core.nav.last.navcomponent + '_' + core.nav.last.navid;
+  const old = core.nav.last.navcomponent + '_' + core.nav.last.navid;
+  const now = params.navcomponent + '_' + params.navid;
 
-    if (core.nav.last.navcomponen === 'table') {
-      core.cache.pages[name] = store.table;
+  if (core.nav.last.navcomponent !== null && core.nav.last.navid !== null) {
+    if (core.nav.last.navcomponent === 'table') {
+      core.cache.pages[old] = store.table;
     }
-    if (core.nav.last.navcomponen === 'options') {
-      core.cache.pages[name] = store.options;
+    if (core.nav.last.navcomponent === 'options') {
+      core.cache.pages[old] = store.options;
     }
-    if (core.nav.last.navcomponen === 'layout') {
-      core.cache.pages[name] = store.layout;
+    if (core.nav.last.navcomponent === 'layout') {
+      core.cache.pages[old] = store.layout;
     }
-    if (core.nav.last.navcomponen === 'template') {
-      core.cache.pages[name] = store.template;
+    if (core.nav.last.navcomponent === 'template') {
+      core.cache.pages[old] = store.template;
     }
   }
 
+  if (core.cache.pages[now] !== undefined) {
+    if (params.navcomponent === 'table') {
+      core.components.table.setData(core.cache.pages[now])
+    }
+    if (params.navcomponent === 'options') {
+      core.components.options.setData(core.cache.pages[now])
+    }
+    if (params.navcomponent === 'layout') {
+      core.components.layout.setData(core.cache.pages[now]);
+    }
+    if (params.navcomponent === 'template') {
+      core.components.template.setData(core.cache.pages[now]);
+    }
+  }
 }
 
 core.events.on('app:page', (params) => {
   cache(params);
   
   core.components.apptabs.addItem({ id: params.navid, label: params.navid, component: params.navcomponent })
-  core.req({ 
-    alias: 'page', 
-    method: 'data', 
-    type: params.navcomponent, 
-    id: params.navid, 
-  })
-  .ok((res) => {
-    if (params.navcomponent === 'table') {
-      core.components.table.setData({ text: params.navid, loading: false })
-    }
-    if (params.navcomponent === 'options') {
-      core.components.options.setData({ text: params.navid, loading: false })
-    }
-    if (params.navcomponent === 'layout') {
-      core.components.layout.setData({ ...res.data, loading: false });
-    }
-    if (params.navcomponent === 'template') {
-      core.components.template.setData({ ...res.data, loading: false });
-    }
-  })
 
-  /*
-  
+  if (core.cache.pages[params.navcomponent + '_' + params.navid] === undefined) {
+    core.req({ 
+      alias: 'page', 
+      method: 'data', 
+      type: params.navcomponent, 
+      id: params.navid, 
+    })
+    .ok((res) => {
+      if (params.navcomponent === 'table') {
+        core.components.table.setData({ text: params.navid, loading: false })
+      }
+      if (params.navcomponent === 'options') {
+        core.components.options.setData({ text: params.navid, loading: false })
+      }
+      if (params.navcomponent === 'layout') {
+        core.components.layout.setData({ ...res.data, loading: false });
+      }
+      if (params.navcomponent === 'template') {
+        core.components.template.setData({ ...res.data, loading: false });
+      }
+    })
+  }
 
-  if (options.component === 'table') {
-    core.components.table.setData({ text: component + ':' + pageid });
-  }
-  if (options.component === 'layout') {
-    core.components.options.setData({ options, ...data  });
-  }
-  if (options.component === 'template') {
-    if (core.nav.last.navid !== null) {
-      core.cache.pages[core.nav.last.navid] = core.store.getState().graph;
-    }
-    if (core.cache.pages[pageid] !== undefined) {
-      core.components.graph.setData({ options, ...core.cache.pages[pageid] });
-    } else {
-      core.components.graph.setData({ options, ...data });
-    }
-  }
-  if (options.component === 'options') {
-    core.components.options.setData({ text: component + ':' + pageid });
-  }
-  */
 });
