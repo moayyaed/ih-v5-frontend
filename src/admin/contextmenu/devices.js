4a3f0ea64d08b5dcdd8eec8735b36a9c275de7cb
Artem Ivanov <fast0490f@mail.ru>
2020-02-06 15:06:13 +0300

    core 3.17.4

--- src/admin/contextmenu/devices.js
+++ src/admin/contextmenu/devices.js
@@ -7,7 +7,7 @@ core.events.on('cm:app:nav:item', (e, params) => {
       { id: '1', type: 'divider', title: 'Edit' },
       { id: '2', type: 'item', icon: 'cut', text: 'Cut', label: '⌘X' },
       { id: '3', type: 'item', icon: 'duplicate', text: 'Copy', label: '⌘C' },
-      { id: '4', type: 'item', icon: 'clipboard', text: 'Paste', label: '⌘V' },
+      { id: '4', type: 'item', icon: 'clipboard', text: 'Paste', label: '⌘V', disabled: true },
     ],
   });
 });
