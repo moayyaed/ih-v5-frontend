e71f8dc75fe73f19e4b27793b1d13729344f2bb8
Artem Ivanov <fast0490f@mail.ru>
2020-02-06 12:13:40 +0300

    core 3.17.4

--- src/admin/network/nav.js
+++ src/admin/network/nav.js
@@ -2,34 +2,30 @@ import core from 'core';
 
 
 core.network.on('#nav', (res, data) => {
-  setTimeout(() => {
-    res([
-      { id: 'lamp_1', component: 'table', title: 'lamp_1' },
-      { id: 'lamp_2', component: 'table', title: 'lamp_2' },
-      { id: 'lamp_3', component: 'table', title: 'lamp_3' },
-    ]);
-  }, 0)
+  res([
+    { id: 'lamp_1', component: 'table', title: 'lamp_1' },
+    { id: 'lamp_2', component: 'table', title: 'lamp_2' },
+    { id: 'lamp_3', component: 'table', title: 'lamp_3' },
+  ]);
 });
 
 core.network.on('#nav:visualization', (res, data) => {
-  setTimeout(() => {
-    res([
-      { id: 'layout', title: 'Экраны', 
-        children: [
-          { id: 'layout_1', component: 'layout', title: 'layout_1' },
-          { id: 'layout_2', component: 'layout', title: 'layout_2' },
-          { id: 'layout_3', component: 'layout', title: 'layout_3' },
-        ]
-      },
-      { id: 'template', title: 'Шаблоны', 
-        children: [
-          { id: 'template_1', component: 'template', title: 'template_1' },
-          { id: 'template_2', component: 'template', title: 'template_2' },
-          { id: 'template_3', component: 'template', title: 'template_3' },
-        ]
-      }
-    ]);
-  }, 0);
+  res([
+    { id: 'layout', title: 'Экраны', 
+      children: [
+        { id: 'layout_1', component: 'layout', title: 'layout_1' },
+        { id: 'layout_2', component: 'layout', title: 'layout_2' },
+        { id: 'layout_3', component: 'layout', title: 'layout_3' },
+      ]
+    },
+    { id: 'template', title: 'Шаблоны', 
+      children: [
+        { id: 'template_1', component: 'template', title: 'template_1' },
+        { id: 'template_2', component: 'template', title: 'template_2' },
+        { id: 'template_3', component: 'template', title: 'template_3' },
+      ]
+    }
+  ]);
 });
 
 core.network.on('#nav:scripts', (res, data) => {
