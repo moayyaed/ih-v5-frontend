dbc2bd7da98b72f58e709a7e0cd62f30374bb6e1
Artem Ivanov <fast0490f@mail.ru>
2020-02-12 12:58:20 +0300

    core 4.0.0

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,48 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 
-import { DragDropContextProvider } from 'react-dnd';
-import HTML5Backend from 'react-dnd-html5-backend';
-
-import AppProgressBar from 'components/AppProgressBar';
-
-import CssBaseline from '@material-ui/core/CssBaseline';
-import ThemeProvider from '@material-ui/styles/ThemeProvider';
-
-import { FocusStyleManager } from '@blueprintjs/core';
-
-import 'normalize.css/normalize.css';
-import "@blueprintjs/core/lib/css/blueprint.css";
-
-import core from 'core';
-import App from 'components/App';
-
-import theme from './theme';
-import options from './options';
-
-import './actions';
-
-// stable
-import './routes';
-import './events';
-import './network';
-import './cache';
-
-
-FocusStyleManager.onlyShowFocusOnTabs();
-
-core.options(options);
 
 ReactDOM.render(
-  <Provider store={core.store}>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <AppProgressBar />
-      <DragDropContextProvider backend={HTML5Backend}>
-        <App />
-      </DragDropContextProvider>
-    </ThemeProvider>
-  </Provider>,
+  <div>TEST</div>,
   document.getElementById('root')
 );
