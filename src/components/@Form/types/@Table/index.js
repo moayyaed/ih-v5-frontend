ca1c24c65577a9d2c5dc079e3defe110054d182c
Artem Ivanov <fast0490f@mail.ru>
2020-02-28 17:33:45 +0300

    core 4.8.5

--- src/components/@Form/types/@Table/index.js
+++ src/components/@Form/types/@Table/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import 'react-base-table/styles.css'
 
@@ -10,7 +10,12 @@ import Menu from 'components/Menu';
 
 import components from './components';
 
+
 const styles = {
+  root: {
+    width: '100%', 
+    height: '100%',
+  },
   rowBasic: {
 
   },
@@ -20,39 +25,6 @@ const styles = {
     height: 2,
     backgroundColor: 'rgba(244, 67, 54, 0.5)',
   }
-}
-
-
-function handleContextMenuBody(event, props) {
-  event.preventDefault();
-  event.stopPropagation();
-
-  const pos = { left: event.clientX, top: event.clientY };
-  const scheme = {
-    main: [
-      { id: 'add', title: 'Add', click: () => handleRowAdd(props) },
-    ]
-  }
-
-  ContextMenu.show(<Menu scheme={scheme} />, pos);
-}
-
-function handleContextMenuRow(props, { event, rowData }) {
-  event.preventDefault();
-  event.stopPropagation();
-  
-  const pos = { left: event.clientX, top: event.clientY };
-  const deleteTitle = props.cache && props.cache.remove && props.cache.remove[rowData.id] ? 
-  'Undelete' : 'Delete';
-
-  const scheme = {
-    main: [
-      { id: 'add', title: 'Add', click: () => handleRowAdd(props, rowData) },
-      { id: 'delete', title: deleteTitle, click: () => handleRowDelete(props, rowData)  }
-    ]
-  }
-
-  ContextMenu.show(<Menu scheme={scheme} />, pos);
 }
 
 
@@ -71,85 +43,110 @@ function getDefault(type) {
   }
 }
 
-function handleRowAdd(props) {
-  const id = props.id;
-  const options = props.options;
-  const row = props.options.columns
-    .reduce((p, c) => {
-      return { ...p, [c.prop]: getDefault(c.type) }
-    }, { id: '__' + shortid.generate() });
 
-  props.onChange(id, options, { op: 'add', row })
-}
+class Table extends Component {
 
-function handleRowDelete(props, row) {
-  const id = props.id;
-  const options = props.options;
+  handleRowAdd = () => {
+    const { id, options } = this.props;
+    const row = options.columns
+      .reduce((p, c) => {
+        return { ...p, [c.prop]: getDefault(c.type) }
+      }, { id: '__' + shortid.generate() });
 
-  props.onChange(id, options, { op: 'delete', row })
-}
+    this.props.onChange(id, options, { op: 'add', row })
+  }
 
+  handleRowDelete = (row) => {
+    const { id, options } = this.props;
+    this.props.onChange(id, options, { op: 'delete', row })
+  }
 
-function Table(props) {
+  handleContextMenuBody = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
   
-  const rowEventHandlers = {
-    onContextMenu: _handleContextMenuRow,
+    const pos = { left: event.clientX, top: event.clientY };
+    const scheme = {
+      main: [
+        { id: 'add', title: 'Add', click: this.handleRowAdd },
+      ]
+    }
+  
+    ContextMenu.show(<Menu scheme={scheme} />, pos);
   }
 
-  function _handleContextMenuRow(data) {
-    handleContextMenuRow(props, data);
+  handleContextMenuRow = ({ event, rowData }) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const { cache } = this.props;
+    const pos = { left: event.clientX, top: event.clientY };
+    const deleteTitle = cache && cache.remove && cache.remove[rowData.id] ? 
+    'Undelete' : 'Delete';
+
+    const scheme = {
+      main: [
+        { id: 'add', title: 'Add', click: this.handleRowAdd },
+        { id: 'delete', title: deleteTitle, click: () => this.handleRowDelete(rowData)  }
+      ]
+    }
+
+    ContextMenu.show(<Menu scheme={scheme} />, pos);
   }
 
-  function Row({ key, index, children, rowid, ...rest }) {
+  renderRow = ({ key, index, children, rowid, ...rest }) => {
     return (
       <div key={key} index={index} {...rest}>
-        <div style={props.cache.remove && props.cache.remove[rowid] ? styles.rowRemove : styles.rowBasic}/>
+        <div style={this.props.cache.remove && this.props.cache.remove[rowid] ? styles.rowRemove : styles.rowBasic}/>
         {children}
       </div>
     )
   }
 
-  function rowProps(props) {
+  rowProps = (props) => {
     return {
-      tagName: Row,
+      tagName: this.renderRow,
       rowid: props.rowData.id,
       index: props.rowIndex,
     }
   }
 
-  return (
-    <div style={{ width: '100%', height: '100%' }} onContextMenu={e => handleContextMenuBody(e, props)}>
-      <AutoResizer>
-        {({ width, height }) => (
-          <BaseTable
-            fixed
-            id={props.id}
-            rowHeight={35}
-            width={width}
-            height={height}
-            data={props.data}
-            options={props.options}
-            onChange={props.onChange}
-            rowProps={rowProps}
-            rowEventHandlers={rowEventHandlers}
-            components={components}
-          >
-            {props.options.columns.map(i => 
-              <Column 
-                {...i}
-                resizable 
-                key={i.prop} 
-                dataKey={i.prop}
-                width={i.width || 150}
-                cache={props.cache}
-              />
-            )}
-          </BaseTable>
-        )}
-      </AutoResizer>
-    </div>
-  )
+  render() {
+    return (
+      <div style={styles.root} onContextMenu={this.handleContextMenuBody}>
+        <AutoResizer>
+          {({ width, height }) => (
+            <BaseTable
+              fixed
+              id={this.props.id}
+              rowHeight={35}
+              width={width}
+              height={height}
+              data={this.props.data}
+              options={this.props.options}
+              onChange={this.props.onChange}
+              rowProps={this.rowProps}
+              rowEventHandlers={{ onContextMenu: this.handleContextMenuRow }}
+              components={components}
+            >
+              {this.props.options.columns.map(i => 
+                <Column 
+                  {...i}
+                  resizable 
+                  key={i.prop} 
+                  dataKey={i.prop}
+                  width={i.width || 150}
+                  cache={this.props.cache}
+                />
+              )}
+            </BaseTable>
+          )}
+        </AutoResizer>
+      </div>
+    )
+  }
 }
+
 
 
 export default Table;
