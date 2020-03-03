4713673ad65fe0773db1e64c00cff15d5ef4753b
Artem Ivanov <fast0490f@mail.ru>
2020-03-03 16:54:22 +0300

    core 4.9.0

--- src/components/AppNav/utils.js
+++ src/components/AppNav/utils.js
@@ -1,21 +1,26 @@
 export function getNodesRange(data, a, b) {
   const temp = {}
-  let start = false;
+  let starta = false;
+  let startb = false;
 
   function nodes(list) {
     for (let item of list) {
-      if (item.id === a) {
-        start = true;
+      if (startb === false && item.id === a) {
+        starta = true;
       }
-      if (item.id === b) {
-        start = true;
+      if (starta === false && item.id === b) {
+        startb = true;
       }
-      if (start) {
+
+      if (starta || startb) {
         temp[item.id] = item;
       }
-      if (item.id === b) {
-        start = false;
-        // break;
+      
+      if (starta && item.id === b) {
+        starta = false;
+      }
+      if (startb && item.id === a) {
+        startb = false;
       }
       if (item.children !== undefined) {
         nodes(item.children);
