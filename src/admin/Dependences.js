d92363ce57279f839ce535ade3914f770d3fa2c2
Artem Ivanov <fast0490f@mail.ru>
2020-03-16 18:11:22 +0300

    core 4.14.7

--- src/admin/Dependences.js
+++ src/admin/Dependences.js
@@ -7,6 +7,10 @@ import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import { DndProvider } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 
+import MultiBackend from 'react-dnd-multi-backend';
+import HTML5ToTouch from 'react-dnd-multi-backend/dist/cjs/HTML5toTouch';
+import { DndProvider as DndProvider2 } from 'react-mosaic-component/node_modules/react-dnd';
+
 import 'typeface-roboto';
 import 'normalize.css/normalize.css';
 
@@ -22,7 +26,9 @@ function Dependences() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <DndProvider backend={HTML5Backend}>
-        <App />
+        <DndProvider2 backend={MultiBackend} options={HTML5ToTouch}>
+          <App />
+        </DndProvider2>
       </DndProvider>
     </ThemeProvider>
   )
