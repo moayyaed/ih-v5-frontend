87434d386d97d96860602c0e86ea789be2edbf3a
Artem Ivanov <fast0490f@mail.ru>
2020-03-30 10:30:50 +0300

    core 4.17.4

--- src/components/@Form/types/@PluginForm1/index.js
+++ src/components/@Form/types/@PluginForm1/index.js
@@ -390,7 +390,7 @@ class PluginForm1 extends Component {
     };
   }
 
-  formRequest = (nodeid, channelview) => {
+  formRequest = (nodeid, channelview, foundPosition = true) => {
     const params = { component: channelview, curent: nodeid };
     
     core
@@ -705,3 +705,26 @@ class PluginForm1 extends Component {
 
 
 export default PluginForm1;
+
+
+/*
+      if (foundPosition) {
+        const node = findNode(this.state.list, nodeid);
+        if (node) {
+          const nodePosition = node.scrollPoint + 21;
+          const curentPosition = this.state.scrollTop > 0 ? this.state.scrollTop : 0;
+          const containerHeight = this.container.clientHeight - 73 - 10;
+
+          console.log(curentPosition, curentPosition + containerHeight - nodePosition)
+          if (node.windowHeight - (this.container.clientHeight - 73) > 0) {
+            // res.scrollTop = node.scrollPoint - ((this.container.clientHeight - 73 - 5) / 2) - 9;
+          }
+          // res.list = editNodes(this.state.list, (item) => {
+          //  if (item.children !== undefined && node.paths[item.id]) {
+          //    return { ...item, expanded: true };
+          //  }
+          //  return item;
+          // }); 
+        }
+      }
+  */
