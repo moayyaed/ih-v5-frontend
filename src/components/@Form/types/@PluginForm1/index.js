5c7c25a84d977dda775ce01ebf7e79ef2f60f7d6
Artem Ivanov <fast0490f@mail.ru>
2020-03-31 09:42:59 +0300

    core 4.18.0

--- src/components/@Form/types/@PluginForm1/index.js
+++ src/components/@Form/types/@PluginForm1/index.js
@@ -853,25 +853,3 @@ class PluginForm1 extends Component {
 
 export default PluginForm1;
 
-
-/*
-      if (foundPosition) {
-        const node = findNode(this.state.list, nodeid);
-        if (node) {
-          const nodePosition = node.scrollPoint + 21;
-          const curentPosition = this.state.scrollTop > 0 ? this.state.scrollTop : 0;
-          const containerHeight = this.container.clientHeight - 73 - 10;
-
-          console.log(curentPosition, curentPosition + containerHeight - nodePosition)
-          if (node.windowHeight - (this.container.clientHeight - 73) > 0) {
-            // res.scrollTop = node.scrollPoint - ((this.container.clientHeight - 73 - 5) / 2) - 9;
-          }
-          // res.list = editNodes(this.state.list, (item) => {
-          //  if (item.children !== undefined && node.paths[item.id]) {
-          //    return { ...item, expanded: true };
-          //  }
-          //  return item;
-          // }); 
-        }
-      }
-  */
---
+++ src/lab/tree.js
@@ -0,0 +1,22 @@
+
+/*
+      if (foundPosition) {
+        const node = findNode(this.state.list, nodeid);
+        if (node) {
+          const nodePosition = node.scrollPoint + 21;
+          const curentPosition = this.state.scrollTop > 0 ? this.state.scrollTop : 0;
+          const containerHeight = this.container.clientHeight - 73 - 10;
+
+          console.log(curentPosition, curentPosition + containerHeight - nodePosition)
+          if (node.windowHeight - (this.container.clientHeight - 73) > 0) {
+            // res.scrollTop = node.scrollPoint - ((this.container.clientHeight - 73 - 5) / 2) - 9;
+          }
+          // res.list = editNodes(this.state.list, (item) => {
+          //  if (item.children !== undefined && node.paths[item.id]) {
+          //    return { ...item, expanded: true };
+          //  }
+          //  return item;
+          // }); 
+        }
+      }
+  */
