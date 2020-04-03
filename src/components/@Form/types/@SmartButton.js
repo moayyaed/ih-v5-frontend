2a2b87159cabff0f6da4e4d88d0f0321c7391f45
Artem Ivanov <fast0490f@mail.ru>
2020-04-03 20:39:18 +0300

    core 4.20.2

--- src/components/@Form/types/@SmartButton.js
+++ src/components/@Form/types/@SmartButton.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import core from 'core';
 
-import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
 import IconButton from '@material-ui/core/IconButton';
 
-import InputAdornment from '@material-ui/core/InputAdornment';
+import Avatar from '@material-ui/core/Avatar';
+import Chip from '@material-ui/core/Chip';
+
 import TextField from '@material-ui/core/TextField';
 
 import LinkIcon from '@material-ui/icons/Link';
@@ -15,8 +18,42 @@ const styles = {
   },
   button: {
     position: 'relative',
+  },
+  avatar: {
+    width: 75,
+    borderRadius: 8,
+    width: 'unset',
+    paddingLeft: 6,
+    paddingRight: 6,
+  },
+  value: {
+    display: 'flex',
+    alignItems: 'center',
+    width: 'calc(100% - 30px)',
+    height: 34,
+  },
+  stub: {
+    color: '#9E9E9E',
+    flexShrink: 0,
+    marginLeft: 6,
+    marginRight: 6,
+  },
+  chip: {
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  },
+  chip2: {
+    borderStyle: 'dashed',
+    flexShrink: 0,
   }
 }
+
+const classes = theme => ({
+  input: {
+    display: 'none'
+  }
+});
 
 function getValue(value) {
   if (value) {
@@ -63,13 +100,31 @@ class Devlink extends Component {
         style={styles.root}
         InputLabelProps={{ shrink: true }} 
         InputProps={{
+          classes: { input: this.props.classes.input },
           endAdornment: (
             <IconButton onClick={this.handleClick} size="small">
               <LinkIcon fontSize="small" />
             </IconButton>
           ),
+          startAdornment: (
+            <div style={styles.value}>
+              <Chip
+                size="small"
+                style={styles.chip}
+                avatar={<Avatar style={styles.avatar}>{this.props.data.value.did}</Avatar>} 
+                label="Имя устройства" 
+              />
+              <div style={styles.stub}>--></div>
+              <Chip
+                size="small"
+                variant="outlined"
+                style={styles.chip2}
+                label={this.props.data.value.prop} 
+              />
+            </div>
+          ),
         }}
-        value={getValue(this.props.data.title)}
+        value=""
         error={this.props.cache.error}
         helperText={this.props.cache.error}
         onClick={this.handleClick}
@@ -78,5 +133,4 @@ class Devlink extends Component {
   }
 }
 
-
-export default Devlink;
+export default withStyles(classes)(Devlink);
