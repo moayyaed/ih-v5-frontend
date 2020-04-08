fb31450b8ba18c0bae27f4b76ad0a849ff7c2a72
Artem Ivanov <fast0490f@mail.ru>
2020-04-08 17:35:11 +0300

    core 4.23.0

--- src/components/@Form/types/@Layout/index.js
+++ src/components/@Form/types/@Layout/index.js
@@ -1,6 +1,12 @@
 import React, { PureComponent } from 'react';
 import core from 'core';
 
+import { 
+  MosaicWithoutDragDropContext as Mosaic, MosaicWindow, 
+  RemoveButton, ExpandButton, Separator, 
+} from 'react-mosaic-component';
+
+
 const styles = {
   root: {
     width: '100%',
@@ -8,12 +14,69 @@ const styles = {
   }
 }
 
+const EMPTY_ARRAY = [];
+
+const TITLES = {
+  layout: 'Layout',
+  toolbar: 'Toolbar',
+}
+
+const state = {
+  windows: {
+    mode: 0,
+    direction: 'row',
+    first: "layout",
+    second: 'toolbar',
+    splitPercentage: 80,
+  },
+}
+
 
 class Layout extends PureComponent {
+  state = state;
+
+  handleChangeWindows = (data) => {
+    this.setState(state => {
+      return { ...state, windows: data };
+    });
+  }
+
+  renderButtons = (id) => {
+    return [];
+  }
+
+  renderDownToolbar = (id) => {
+    return null;
+  }
+
+  renderComponent = (id) => {
+    return null;
+  }
+
   render() {
     return (
       <div style={styles.root}>
-        LAYOUT
+        <Mosaic
+          className="mosaic-blueprint-theme"
+          value={this.state.windows}
+          onChange={this.handleChangeWindows}
+          renderTile={(id, path) => {
+            return (
+              <MosaicWindow
+                key={id}
+                draggable={false}
+                title={TITLES[id]}
+                additionalControls={EMPTY_ARRAY}
+                path={path}
+                renderToolbar={null}
+                toolbarControls={this.renderButtons(id)}
+                renderPreview={() => this.renderDownToolbar(id)}
+              >
+                {this.renderComponent(id)}
+              </MosaicWindow>
+            )
+          }}
+        />
       </div>
     )
   }
