bfcdf98a488adf2ddd675644925678b7a5846e0b
Artem Ivanov <fast0490f@mail.ru>
2020-04-14 20:57:15 +0300

    core 4.25.16

--- src/components/@Form/types/@Layout/index.js
+++ src/components/@Form/types/@Layout/index.js
@@ -59,7 +59,8 @@ class Layout extends PureComponent {
         section: null,
         column: null,
       },
-      list: [],
+      /*
+      list: ['s1', 's2', 's3', 's4'],
       sections: {
         s1: { height: 50, columns: ['s1_c1', 's1_c2', 's1_c3'] },
         s2: { height: 75, columns: ['s2_c1', 's2_c2', 's2_c3'] },
@@ -78,6 +79,7 @@ class Layout extends PureComponent {
         s4_c2: { type: null },
         s4_c3: { type: null },
       },
+      */
     });
   }
 
