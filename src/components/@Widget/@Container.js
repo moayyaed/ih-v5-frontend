454e2ab5d3a2896613a5d327e19d895e0031c780
Artem Ivanov <fast0490f@mail.ru>
2020-04-29 20:17:34 +0300

    core 4.28.27

--- src/components/@Widget/@Container.js
+++ src/components/@Widget/@Container.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react';
 import core from 'core';
 
 
+const styles = {
+  root: {
+    width: '100%',
+    height: '100%',
+  }
+}
+
 class Container extends Component {
+  state = { settings: {}, list: [], elements: {} }
+
   componentDidMount() {
     if (this.props.params.containerId.id) {
       this.handleRequest(this.props.params.containerId.id)
@@ -13,16 +22,32 @@ class Container extends Component {
     core
       .request({ method: 'widget_container', params: id })
       .ok(res => {
-        console.log(res);
+        this.setState(state => {
+          return { ...state, ...res }
+        })
       })
   }
 
   render() {
     return (
-      <>
-        <div>{this.props.id}</div>
-        <div>{this.props.params.type}</div>
-      </>
+      <div style={styles.root}>
+        {this.state.list
+          .map(key => {
+            const element = this.state.elements[key]
+            return (
+              <div
+                key={key} 
+                style={{
+                  position: 'absolute',
+                  transform: `translate3d(${element.x}px, ${element.y}px, 0px)`,
+                  width: element.w,
+                  height: element.h,
+                  border: `1px solid ${element.borderColor}`
+                }} 
+              />
+            )
+          })}
+      </div>
     )
   }
 }
