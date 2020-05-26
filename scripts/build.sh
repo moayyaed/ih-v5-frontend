d118b459926ae1b2b4a96dec94054a68a1070123
Artem Ivanov <fast0490f@mail.ru>
2020-05-26 17:17:00 +0300

    core 4.33.0

---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -fr build/*
+rm -fr builds/*
+mkdir -p builds/user
+mkdir -p builds/admin
+
+cp scripts/user.js src/index.js && react-scripts build
+cp -a build/* builds/user
+
+rm -fr build/*
+
+cp scripts/admin.js src/index.js && react-scripts build
+cp -a build/* builds/admin
---
+++ src/@user/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+  <div>USER_INTERFACE_CORE_ENABLED</div>,
+  document.getElementById('root')
+);
