4be27875572b651497f0c6a39be3a212b52d6bd2
Artem Ivanov <fast0490f@mail.ru>
2020-06-26 12:48:26 +0300

    core 4.37.6

--- src/components/AppLayout/index.js
+++ src/components/AppLayout/index.js
@@ -6,7 +6,8 @@ import { createSelector } from 'reselect';
 
 import { withStyles } from '@material-ui/core/styles';
 
-import widgets from 'components/@Widgets';
+import ReactResizeDetector from 'react-resize-detector';
+import elemets from 'components/@Elements';
 
 
 const styles = {
@@ -44,11 +45,86 @@ class AppLayout extends Component {
 
   }
 
+  handleRender = (id, item, scaleW, scaleH) => {
+    console.log(scaleW, scaleH)
+    if (item.type === 'group') {
+      return (
+        <div
+          key={id}
+          style={{ 
+            position: 'absolute', 
+            left: item.x * scaleW,
+            top: item.y * scaleH,
+            width: item.w * scaleW,
+            height: item.h * scaleH,
+          }}
+        >
+          {item.elements.map(cid => this.handleRender(cid, this.props.state.layout.elements[cid], scaleW, scaleH))}
+        </div>
+      )
+    }
+    if (item.type === 'template') {
+      return (
+        <div
+          key={id}
+          style={{ 
+            position: 'absolute', 
+            left: item.x * scaleW,
+            top: item.y * scaleH,
+            width: item.w * scaleW,
+            height: item.h * scaleH,
+          }}
+        >
+          {elemets(this.props.state.layout.elements[id].type, { item: this.props.state.layout.elements[id], template: this.props.state.templates[item.templateId] })}
+        </div>
+      )
+    }
+    if (item.type === 'container') {
+      return (
+        <div
+          key={id}
+          style={{ 
+            position: 'absolute', 
+            left: item.x * scaleW,
+            top: item.y * scaleH,
+            width: item.w * scaleW,
+            height: item.h * scaleH,
+          }}
+        >
+          {elemets(this.props.state.layout.elements[id].type, { item: this.props.state.layout.elements[id], container: this.props.state.containers[this.props.state.layout.elements[id].containerId.id], templates: this.props.state.templates })}
+        </div>
+      )
+    }
+    return (
+      <div
+        key={id}
+        style={{ 
+          position: 'absolute', 
+          left: item.x * scaleW,
+          top: item.y * scaleH,
+          width: item.w * scaleW,
+          height: item.h * scaleH,
+        }}
+      >
+        {elemets(this.props.state.layout.elements[id].type, { item: this.props.state.layout.elements[id] })}
+      </div>
+    )
+  }
+
   render({ id, route, state, auth, classes } = this.props) {
     return (
-      <div style={styles.root}>
-        USER_INTERFACE
-      </div>
+        <ReactResizeDetector handleWidth handleHeight>
+          {({ width, height }) => {
+            if (width && state.layout.settings) {
+              return (
+                <div style={styles.root}>
+                  {state.layout.list.map(id => this.handleRender(id, state.layout.elements[id], width / state.layout.settings.w, height / state.layout.settings.h))}
+                </div>
+              )
+            }
+            return <div />;
+          }}
+        </ReactResizeDetector>
     );
   }
 }
