3bb89235d9c68030931562d5471e9a44e0ac3050
Artem Ivanov <fast0490f@mail.ru>
2020-06-30 18:30:22 +0300

    core 4.38.4

--- src/components/@Elements/@Container.js
+++ src/components/@Elements/@Container.js
@@ -46,7 +46,7 @@ class Container extends PureComponent {
           top: item.y,
           width: item.w,
           height: item.h,
-          zIndex: props.item.zIndex,
+          zIndex: item.zIndex,
         }}
       >
         {elemets(this.props.container.elements[id].type, { item: this.props.container.elements[id] })}
