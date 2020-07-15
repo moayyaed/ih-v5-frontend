c169c2b9e4cebdc22e81178b82a107370e33e33f
Artem Ivanov <fast0490f@mail.ru>
2020-07-15 13:12:49 +0300

    core 4.43.2

--- src/components/@Form/types/@InputLink.js
+++ src/components/@Form/types/@InputLink.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 
 import TextField from '@material-ui/core/TextField';
+
+import IconButton from '@material-ui/core/IconButton';
+
+import LinkIcon from '@material-ui/icons/Link';
+import LinkOffIcon from '@material-ui/icons/LinkOff';
 
 const styles = {
   root: {
@@ -8,21 +13,41 @@ const styles = {
   }
 }
 
-function InputLink(props) {
-  console.log(props.route.type)
-  return (
-    <TextField
-      id={props.options.id} 
-      label={props.options.title} 
-      style={styles.root}
-      InputLabelProps={{ shrink: true, style: props.getStyle(props) }} 
-      value={props.data.value}
-      error={props.cache && props.cache.error}
-      helperText={props.cache && props.cache.error}
-      onChange={(e) => props.onChange(props.id, props.options, null, { value: e.target.value })}
-    />
-  )
+class InputLink extends PureComponent {
+  handleChangeText = (e) => {
+    this.props.onChange(this.props.id, this.props.options, null, { value: e.target.value })
+  }
+
+  handleClickButton = () => {
+    if (this.props.data.link) {
+      this.props.onChange(this.props.id, this.props.options, null, { link: null, var: null, text: null, value: this.props.data.text || 'Text 1' })
+    } else {
+      this.props.onChange(this.props.id, this.props.options, null, { link: 'state', var: 'state1', value: 'state', text: this.props.data.value })
+    }
+  }
+
+  render() {
+    return (
+      <TextField
+        id={this.props.options.id} 
+        label={this.props.options.title} 
+        style={styles.root}
+        disabled={Boolean(this.props.data.link)}
+        InputProps={{
+          endAdornment: (
+            this.props.route.type ? <IconButton onClick={this.handleClickButton} size="small">
+             {this.props.data.link ? <LinkOffIcon fontSize="small" /> : <LinkIcon fontSize="small" />}
+            </IconButton> : null
+          ),
+        }}
+        InputLabelProps={{ shrink: true, style: this.props.getStyle(this.props) }} 
+        value={this.props.data.value}
+        error={this.props.cache && this.props.cache.error}
+        helperText={this.props.cache && this.props.cache.error}
+        onChange={this.handleChangeText}
+      />
+    )
+  }
 }
 
-
-export default React.memo(InputLink);
+export default InputLink;
