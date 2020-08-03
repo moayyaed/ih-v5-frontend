49b0d6affb244f6fcc21b94eba2bf0da108ae06a
Artem Ivanov <fast0490f@mail.ru>
2020-08-03 10:23:04 +0300

    core 4.46.6

--- src/components/@Form/types/@List.js
+++ src/components/@Form/types/@List.js
@@ -18,6 +18,18 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: 360,
     backgroundColor: theme.palette.background.paper,
   },
+  title: {
+    border: '1px solid #F5F5F5',
+    height: 35,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontWeight: 600,
+    color: '#394b59',
+    boxShadow: '0 1px 1px rgba(16, 22, 26, 0.15)',
+    zIndex: 4,
+    borderRadius: 3,
+  }
 }));
 
 
@@ -25,32 +37,33 @@ function _List(props) {
   const classes = useStyles();
 
   return (
-    <List 
-      className={classes.root}
-    >
-      {props.options.data.map(item => {
-        return (
-          <ListItem 
-            key={item.id} 
-            role={undefined} 
-            dense button 
-            onClick={() => props.onChange(props.id, props.options, null, props.data.id === item.id ? {} : item)}
-          >
-            <ListItemIcon>
-              <Checkbox
-                checked={props.data.id === item.id}
-                color="primary"
-                edge="start"
-                tabIndex={-1}
-                disableRipple
-                inputProps={{ 'aria-labelledby': item.title }}
-              />
-            </ListItemIcon>
-            <ListItemText id={item.title} primary={item.title} />
-          </ListItem>
-        );
-      })}
-    </List>
+    <>
+      <div className={classes.title}>{props.options.title}</div>
+      <List className={classes.root} >
+        {props.options.data.map(item => {
+          return (
+            <ListItem 
+              key={item.id} 
+              role={undefined} 
+              dense button 
+              onClick={() => props.onChange(props.id, props.options, null, props.data.id === item.id ? {} : item)}
+            >
+              <ListItemIcon>
+                <Checkbox
+                  checked={props.data.id === item.id}
+                  color="primary"
+                  edge="start"
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{ 'aria-labelledby': item.title }}
+                />
+              </ListItemIcon>
+              <ListItemText id={item.title} primary={item.title} />
+            </ListItem>
+          );
+        })}
+      </List>
+    </>
   )
 }
 
