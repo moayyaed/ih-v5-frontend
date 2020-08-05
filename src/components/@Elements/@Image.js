981a738f9326575ba64be1dc351485d8b1b5440e
Artem Ivanov <fast0490f@mail.ru>
2020-08-05 17:58:27 +0300

    core 4.48.10

--- src/components/@Elements/@Image.js
+++ src/components/@Elements/@Image.js
@@ -36,6 +36,7 @@ function Image(props) {
         boxShadow: props.item.boxShadow.active ? props.item.boxShadow.value : 'unset',
         transform: transform(props.item),
         animation: props.item.animation.active ? props.item.animation.value : 'unset',
+        overflow: 'hidden',
       }}
     >
       <div
--- src/components/@Elements/@TextImage.js
+++ src/components/@Elements/@TextImage.js
@@ -104,6 +104,7 @@ function Text(props) {
         boxShadow: props.item.boxShadow.active ? props.item.boxShadow.value : 'unset',
         transform: transform(props.item),
         animation: props.item.animation.active ? props.item.animation.value : 'unset',
+        overflow: 'hidden',
       }}
     >
       <div
