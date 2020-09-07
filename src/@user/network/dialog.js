cbecc653ebb36d9461f74873745530ca3ca649b5
Artem Ivanov <fast0490f@mail.ru>
2020-09-07 13:13:17 +0300

    core 4.62.7

--- src/@user/network/dialog.js
+++ src/@user/network/dialog.js
@@ -1,5 +1,57 @@
 import core from 'core';
 
+function preparationData(data) {
+  let count = 0;
+  const expands = [];
+  const itemsX = {};
+  const itemsY = {};
+
+  Object
+    .keys(data.elements)
+    .forEach(id => {
+      const item = data.elements[id];
+      if (item.type === 'expand') {
+        expands.push({ x: item.x.value, y: item.y.value, w: item.w.value, h: item.h.value });
+      } else {
+        if (itemsX[item.x.value] === undefined) {
+          itemsX[item.x.value] = [];
+        }
+        itemsX[item.x.value].push(id);
+
+        if (itemsY[item.y.value] === undefined) {
+          itemsY[item.y.value] = [];
+        }
+        itemsY[item.y.value].push(id);
+      }
+    });
+    if (expands.length) {
+      expands.reverse();
+      let bottom1 = data.settings.h.value;
+      let bottom2 = document.body.clientHeight
+
+      expands.forEach(i => {
+        const y = i.y + i.h;
+        const items = [];
+
+        Object
+          .keys(itemsY)
+          .forEach(id => {
+            if (Number(id) >= y) {
+              itemsY[id].forEach(i => items.push(i))
+            }
+          });
+        if (items.length) {
+          const offset =  bottom2 - bottom1;
+          items.forEach(id => {
+            data.elements[id].y.value = data.elements[id].y.value + offset;
+          });
+          bottom1 = 0;
+          bottom2 = 0;
+        }
+      });
+    }
+  return data;
+}
 
 core.network.request('applayout_dialog', (send, context) => {
   send({ api: 'dialog', id: context.params.id },);
@@ -7,5 +59,5 @@ core.network.request('applayout_dialog', (send, context) => {
 
 
 core.network.response('applayout_dialog', (answer, res, context) => {
-  answer(res.data);
+  answer(preparationData(res.data));
 })
