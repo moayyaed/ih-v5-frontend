058101e4352d398956b7520c128866380113b046
Artem Ivanov <fast0490f@mail.ru>
2020-09-07 17:02:19 +0300

    core 4.62.8

--- src/@user/network/dialog.js
+++ src/@user/network/dialog.js
@@ -2,54 +2,43 @@ import core from 'core';
 
 function preparationData(data) {
   let count = 0;
-  const expands = [];
+  let step = 0;
+
   const itemsX = {};
   const itemsY = {};
+  const delta = document.body.clientHeight - data.settings.h.value;
 
   Object
     .keys(data.elements)
     .forEach(id => {
       const item = data.elements[id];
+      if (itemsY[item.y.value] === undefined) {
+        itemsY[item.y.value] = [];
+      }
       if (item.type === 'expand') {
-        expands.push({ x: item.x.value, y: item.y.value, w: item.w.value, h: item.h.value });
+        itemsY[item.y.value].push(item.type);
+        count++;
       } else {
-        if (itemsX[item.x.value] === undefined) {
-          itemsX[item.x.value] = [];
-        }
-        itemsX[item.x.value].push(id);
-
-        if (itemsY[item.y.value] === undefined) {
-          itemsY[item.y.value] = [];
-        }
         itemsY[item.y.value].push(id);
       }
     });
-    if (expands.length) {
-      expands.reverse();
-      let bottom1 = data.settings.h.value;
-      let bottom2 = document.body.clientHeight
 
-      expands.forEach(i => {
-        const y = i.y + i.h;
-        const items = [];
-
-        Object
-          .keys(itemsY)
-          .forEach(id => {
-            if (Number(id) >= y) {
-              itemsY[id].forEach(i => items.push(i))
-            }
-          });
-        if (items.length) {
-          const offset =  bottom2 - bottom1;
-          items.forEach(id => {
+    Object
+    .keys(itemsY)
+    .forEach(id => {
+      if (itemsY[id].includes('expand')) {
+        step++;
+      }
+      if (step) {
+        itemsY[id].forEach(id => {
+          if (id !== 'expand') {
+            const offset = delta * (step / count)
             data.elements[id].y.value = data.elements[id].y.value + offset;
-          });
-          bottom1 = 0;
-          bottom2 = 0;
-        }
-      });
-    }
+          }
+        })
+      }
+    })
+
   return data;
 }
 
