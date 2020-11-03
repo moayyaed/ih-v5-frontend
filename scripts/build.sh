ce1c643f5652c4a0bc65579a5ea9d6c740967c04
Artem Ivanov <fast0490f@mail.ru>
2020-11-03 16:20:58 +0300

    core 4.68.0

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,10 +5,10 @@ rm -fr builds/*
 mkdir -p builds/user
 mkdir -p builds/admin
 
-cp scripts/user.js src/index.js && react-scripts build
+cp scripts/user.js src/index.js && export NODE_OPTIONS="--max-old-space-size=8192" && react-scripts build
 cp -a build/* builds/user
 
 rm -fr build/*
 
-cp scripts/admin.js src/index.js && react-scripts build
+cp scripts/admin.js src/index.js && export NODE_OPTIONS="--max-old-space-size=8192" && react-scripts build
 cp -a build/* builds/admin
