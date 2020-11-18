7a7726233fb36d6536b226b5d7929d9ae58479dc
Artem Ivanov <fast0490f@mail.ru>
2020-11-18 13:42:59 +0300

    core 4.68.54

--- src/components/schemes/dynamic.js
+++ src/components/schemes/dynamic.js
@@ -1,5 +1,5 @@
 const dynamic = {
-  chart: { variant: 'tree', title: 'Выберите свойство', id: 'devicedn', save: false },
+  chart: { variant: 'tree', title: 'Выберите свойство', id: 'devicesdn', save: false },
   chart_multi: { variant: 'options',title: 'Выберите график', id: 'charts' },
   devicelog: { variant: 'tree',title: 'Выберите свойство', id: 'visitemsAndVistemplates' },
   devicesettings: { variant: 'tree',title: 'Выберите свойство', id: 'visitemsAndVistemplates' },
