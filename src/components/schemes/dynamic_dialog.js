d96ab02bf4cf2ab0f320799d7588cd4285f5c7c3
Artem Ivanov <fast0490f@mail.ru>
2020-11-25 16:22:57 +0300

    core 4.68.73

--- src/components/schemes/dynamic_dialog.js
+++ src/components/schemes/dynamic_dialog.js
@@ -1,9 +1,9 @@
 const dynamic_dialog = {
-  chart: { variant: 'tree', title: 'Выберите свойство', id: 'devicesdn', save: false },
+  chart: { variant: 'tree', title: 'Выберите свойство', id: 'devpropsAndAny', save: false },
   chart_multi: { variant: 'options',title: 'Выберите график', id: 'charts' },
-  devicelog: { variant: 'tree',title: 'Выберите свойство', id: 'visitemsAndVistemplates' },
-  devicesettings: { variant: 'tree',title: 'Выберите свойство', id: 'visitemsAndVistemplates' },
-  slider: { variant: 'tree',title: 'Выберите свойство', id: 'visitemsAndAny' },
+  devicelog: { variant: 'tree',title: 'Выберите свойство', id: 'devicesAndAny' },
+  devicesettings: { variant: 'tree',title: 'Выберите свойство', id: 'devicesAndAny' },
+  slider: { variant: 'tree',title: 'Выберите свойство', id: 'devpropsAndAny' },
 };
 
 
