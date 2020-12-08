4ccf75eedf82a9e12f1cab1281b8c7cdc1b3ddc7
Artem Ivanov <fast0490f@mail.ru>
2020-12-08 18:51:44 +0300

    core 4.61.2

--- src/components/AppLayout/index.js
+++ src/components/AppLayout/index.js
@@ -57,11 +57,11 @@ class AppLayout extends Component {
   commandLayout = (data) => {
     if (data.command === 'gotolayout') {
       if (this.props.state.layoutId === data.id) {
-        if (data.target_container_id && data.target_frame && data.target_container_id.id && data.target_frame.id) {
+        if (data.target_container_id && data.target_frame) {
           core
-          .request({ method: 'get_container', params: data.target_container_id.id })
+          .request({ method: 'get_container', params: data.target_container_id })
           .ok(res => {
-            const container = this.props.state.layout.elements[data.target_frame.id];
+            const container = this.props.state.layout.elements[data.target_frame];
             if (container) {
               core.tunnel.unsub({ 
                 method: 'unsub',
@@ -71,14 +71,14 @@ class AppLayout extends Component {
               }, this.subs[container.widgetlinks.link.id]);
               delete this.subs[container.widgetlinks.link.id]
 
-              this.subs[data.target_container_id.id] = (realtime) => this.realtimeContainer(data.target_container_id.id, realtime)
+              this.subs[data.target_container_id] = (realtime) => this.realtimeContainer(data.target_container_id, realtime)
               core.tunnel.sub({ 
                 method: 'sub',
                 type: 'container',
-                uuid: data.target_container_id.id,
-                id: data.target_container_id.id,
-              }, this.subs[data.target_container_id.id]);
-              core.actions.layout.changeContainer(data.target_frame.id, data.target_container_id.id, res);
+                uuid: data.target_container_id,
+                id: data.target_container_id,
+              }, this.subs[data.target_container_id]);
+              core.actions.layout.changeContainer(data.target_frame, data.target_container_id, res);
             }
           });
         }
