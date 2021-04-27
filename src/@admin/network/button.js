c48703c90fe531baebde81425b27b3dee4642ce0
Artem Ivanov <fast0490f@mail.ru>
2021-04-27 09:49:54 +0300

    core 4.68.28

--- src/@admin/network/button.js
+++ src/@admin/network/button.js
@@ -18,3 +18,22 @@ core.network.request('button_command', (send, context) => {
 core.network.response('button_command', (answer, res, context) => {
   answer(res);
 })
+
+
+core.network.request('button_table_command', (send, context) => {
+  send({ 
+    method: 'row_command', 
+    type: 'form',
+    id: context.params.id,
+    command: context.params.command,
+    param: context.params.param,
+    nodeid: context.params.nodeid,
+    subnodeid: context.params.subnodeid,
+    payload: context.payload,
+  });
+})
+
+
+core.network.response('button_table_command', (answer, res, context) => {
+  answer(res);
+})
--- src/components/@Form/types/@Table/Button.js
+++ src/components/@Form/types/@Table/Button.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import core from 'core';
+
 import Button from '@material-ui/core/Button';
 
 const styles = {
@@ -18,10 +20,44 @@ const styles = {
 }
 
 
+function handleClick(props) {
+  const store = core.store.getState().apppage;
+
+  const id = props.container.props.id;
+  const route = props.container.props.route;
+  const options = props.container.props.options;
+  const column = props.column;
+  const row = props.rowData;
+
+  const params = {
+    subnodeid: route.channel,
+    nodeid: route.nodeid,
+    command: props.cellData.command,
+    param: options.param,
+    id: store.component[0].id,
+  };
+
+
+  core
+  .request({ method: 'button_table_command', params, payload: row })
+  .ok(res => {
+    if (res.alert) {
+      core.actions.app.alertOpen(res.alert || 'info', res.message || '');
+    }
+    if (res.refresh) {
+      core.transfer.send('refresh_content');
+    }
+    if (res.restart) {
+      core.actions.app.restart(true);
+    }
+  });
+
+}
+
 function TableButtonComponent(props) {
   return (
     <div style={styles.root} className={props.className}>
-      <Button style={styles.button} variant="contained" color="primary">
+      <Button style={styles.button} variant="contained" color="primary" onClick={() => handleClick(props)}>
         {props.cellData.title || ''}
       </Button>
     </div>
