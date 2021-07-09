b1e68303f303cfc756c2e2059293ed8a06da2b6d
Artem Ivanov <fast0490f@mail.ru>
2021-07-09 10:44:38 +0300

    version 5.3.0

--- src/components/schemes/dynamic_layout.js
+++ src/components/schemes/dynamic_layout.js
@@ -15,7 +15,7 @@ const dynamic_layout = {
   slider_pretto: { variant: 'tree', title: 'Выберите свойство', id: 'devprops', save: false },
   slider_airbnb: { variant: 'tree', title: 'Выберите свойство', id: 'devprops', save: false },
   checkbox: { variant: 'tree', title: 'Выберите свойство', id: 'devprops', save: false },
-  container: { variant: 'tree', title: 'Выберите контейнер', id: 'viscontx' },
+  container: { variant: 'options', title: 'Выберите контейнер', id: 'viscontx' },
   journal: { variant: 'tree', title: 'Выберите журнал', id: 'journals' },
   alertlog: { variant: 'tree', title: 'Выберите журнал тревог', id: 'alertjournals' },
 };
